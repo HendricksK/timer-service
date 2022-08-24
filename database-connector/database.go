63dcfa8a1bce5020b32b718101616f15167df72a
HendricksK <kurvinhendricks@gmail.com>
2022-08-24 23:16:03 +0200

    EOD, adding new ticket for baseline ORM work inside database package, will get to that eventually

--- database-connector/database.go
+++ database-connector/database.go
@@ -42,3 +42,53 @@ func GetPostgresDatabaseHandler() *sql.DB {
 func CloseDBConnection(db *sql.DB) error {
 	return db.Close()
 }
+
+// Will be using this to see how viable it would be to create a custom ORM
+// https://trello.com/c/gQyqoozM
+
+// func Update() struct {
+// 	row := db.QueryRow("SELECT id FROM "+tableName+" WHERE ref = $1", update.Ref)
+
+// 	if row == nil {
+// 		database.CloseDBConnection(db)
+// 		return timer
+// 	}
+
+// 	row.Scan(&update.Id)
+
+// 	// iterate over timer struct to create prepared update, do not want to update fields that we do not have
+// 	// Very helpful in my persuit https://www.golangprograms.com/how-to-get-struct-variable-information-using-reflect-package.html
+// 	// Can build all of this into a function in the database package
+// 	updateReflection := reflect.ValueOf(&update).Elem()
+
+// 	var fieldsBeingUpdated string
+// 	noUpdatesAllowed := []string{"Id", "Ref"}
+// 	for i := 0; i < updateReflection.NumField(); i++ {
+
+// 		if updateReflection.Type().Field(i).Name != "" && !slices.Contains(noUpdatesAllowed, updateReflection.Type().Field(i).Name) && updateReflection.Field(i).Interface() != "" {
+// 			// https://yourbasic.org/golang/fmt-printf-reference-cheat-sheet/
+// 			// want to pass the value type, and then parse in the actual value
+// 			// Will need to look into this to ensure we cannot SQL inject.
+// 			// What we can do is rather build out an array of no type (hopefully and add values to that, only the values we want to update)
+// 			if strings.Contains(updateReflection.Type().Field(i).Type.String(), "int") { // integer
+// 				fieldsBeingUpdated += fmt.Sprintf("%v = %v,", strings.ToLower(updateReflection.Type().Field(i).Name), updateReflection.Field(i).Int())
+// 			}
+
+// 			if strings.Contains(updateReflection.Type().Field(i).Type.String(), "string") { // string
+// 				fieldsBeingUpdated += fmt.Sprintf("%v = '%s',", strings.ToLower(updateReflection.Type().Field(i).Name), sanitize.XSS(updateReflection.Field(i).String()))
+// 			}
+// 		}
+// 	}
+
+// 	fieldsBeingUpdated = strings.TrimRight(fieldsBeingUpdated, ",")
+
+// 	sqlStatement := `
+// 		UPDATE ` + tableName + `
+// 		SET ` + fieldsBeingUpdated + `
+// 		WHERE ref = $1`
+
+// 	// Need to get all non empty fields and then drop them in preparedQuery
+// 	// https://stackoverflow.com/questions/49965387/use-slice-reflect-type-as-map-key
+// 	updateResult, updateErr := db.Exec(sqlStatement, update.Ref)
+// 	// updateResult, updateErr := db.Query(sqlStatement, update.Ref)
+// }
